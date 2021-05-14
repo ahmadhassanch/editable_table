419071c3ba8e7eaeaa8169d48501d1744f719417
Ahmad Hassan <ahmadhassanch@hotmail.com>
2021-05-14 23:07:58 +0500

    Very small functions to extract_tables, rows, and columns -- working.

--- pdf/simple_parser/main.py
+++ pdf/simple_parser/main.py
@@ -1,5 +1,6 @@
 from extract_tables import extract_tables
 from extract_rows import extract_rows
+from extract_cols import extract_cols
 
 def main():
 	f = open("base1.html", "r")
@@ -11,8 +12,11 @@ def main():
 	table = objList[1]["value"]
 	print("++++++++++++++++++=")
 
-	extract_rows(table)
+	rows = extract_rows(table)
 
-
+	for row in rows:
+		print("============================")
+		print("row:", row)
+		cols = extract_cols(row["data"])
 
 main()
