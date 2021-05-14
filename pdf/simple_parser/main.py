ac749baadf33bd0ae59d3c9a0f35db733d455e99
Ahmad Hassan <ahmadhassanch@hotmail.com>
2021-05-14 21:42:01 +0500

    Was teaching my kid also, but have done a basic implementation of splitting data in table and non-table

--- pdf/simple_parser/main.py
+++ pdf/simple_parser/main.py
@@ -1,15 +1,42 @@
 
-def extract_table(st):
-	objectList = [];
+def extract_table(objectList, st):
 	val_start = st.find("<table")
-	print(val_start)
 
+	if val_start == -1:
+		return objectList
+	# print(val_start)
+
+	text = st[:val_start].strip()
+	if (text !=""):
+		objectList.append({"type" : "text", "value" : text})
+		# print(text)
+
+	val_end = st.find("</table>")
+	if val_start == -1:
+		print(">>>>>>>>>>>>> Error <<<<<<<<<<<, should get </table>")
+		exit()
+
+	tableData = st[val_start: val_end+8].replace("\n", " ")
+	objectList.append({"type" : "table", "value" : tableData})
+	# print(text)
+
+
+	return objectList, st[val_end+8:]
 
 def main():
-	#f = open("testcase.html", "r")
+	objectList = [];
 	f = open("base1.html", "r")
 	st = f.read()
 	# print(st)
-	extract_table(st)
+	for i in range(2):
+		objList, st = extract_table(objectList, st)
+	
+	for objs in objList:
+		if objs["type"] == 'text':
+			print("text", objs["value"])
+		if objs["type"] == 'table':
+			print("TABLE", objs["value"][:20], "...", objs["value"][-20:])
+	# print(objList)
+
 
 main()
