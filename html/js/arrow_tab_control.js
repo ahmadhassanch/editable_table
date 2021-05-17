a6fffe95bea4e598bc1686065ac1921285c6674b
Ahmad Hassan <ahmadhassanch@hotmail.com>
2021-05-18 04:39:55 +0500

    Now going to work on arrow up and down function.

--- html/js/arrow_tab_control.js
+++ html/js/arrow_tab_control.js
@@ -2,6 +2,24 @@ shiftStatus = false;
 
 document.onkeyup = function (e) {if (e.key == "Shift") shiftStatus = false;}
 
+function handleArrowKey(e, offset){
+    var node = mCont.tableController.getSelectedElement();
+    if ((node == undefined) || (node.nodeName != "TD")){
+        console.log("Wrong element selected or no selection");
+        return;
+    }
+
+    // if (shiftStatus == true){
+    //     var s = node.previousSibling;
+    //     if (s==null)
+    //         s = node.parentNode.previousSibling.lastChild;
+    // }
+
+    var col = node.cellIndex;
+    var row = node.parentNode.rowIndex;
+    console.log("Arrow", offset, row, col);
+}
+
 function handleTabKey(e){
     var node = mCont.tableController.getSelectedElement();
     if ((node == undefined) || (node.nodeName != "TD")){
@@ -25,7 +43,6 @@ function handleTabKey(e){
     range.setEnd(s.firstChild, s.firstChild.length);
     window.getSelection().removeAllRanges();
     window.getSelection().addRange(range);
-    e.preventDefault();    
 }
 
 document.onkeydown = function (e) {
@@ -33,17 +50,26 @@ document.onkeydown = function (e) {
     key = e.key;
     console.log("dn key code: " + kc + ", key: " + e.key);
 
-    if (e.key == "Shift"){ 
-        shiftStatus = true; 
-        return; 
+    switch(e.key){
+        case "Shift": 
+            shiftStatus = true; 
+            return; 
+
+        case "Tab": 
+            handleTabKey(e);
+            e.preventDefault();   
+            return; 
+
+        case "ArrowDown": 
+            handleArrowKey(e, 1);
+            e.preventDefault();   
+            return;
+
+        case "ArrowUp": 
+            handleArrowKey(e, -1);
+            e.preventDefault();   
+            return;
     }
-    
-    if (e.key == "Tab"){
-        handleTabKey(e);
-        return;
-    }
-    // var col = node.cellIndex;
-    // var row = node.parentNode.rowIndex;
 }
 
 // var selection = window.getSelection();  
