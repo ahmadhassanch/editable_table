4287f5c3a17d5e8ae266c9dc019a489797e1d9f1
Ahmad Hassan <ahmadhassanch@hotmail.com>
2021-05-20 18:36:16 +0500

    class lecture

--- test/js/main.js
+++ test/js/main.js
@@ -5,9 +5,15 @@ console.log(els);
 
 for (var i=0;i<els.length;i++)
 {
+
     els[i].oncontextmenu = function(e){
     e.preventDefault();    
     console.log(e.target);
+    // toggleMenu(e);
+    var contextMenu = document.getElementById("context-menu");
+    e.target.style.position = "relative";
+    contextMenu.style.display = 'block'
+    e.target.appendChild(contextMenu);
 
     }    
 }
@@ -18,8 +24,24 @@ function toggleMenu(e){
     console.log(contextMenu);
     console.log("before", contextMenu.style.display);
     if(contextMenu.style.display == 'none')
-        contextMenu.style.display = 'block'
+        contextMenu.style.display = 'block';
     else
-        (contextMenu.style.display = 'none')
+        contextMenu.style.display = 'none';
     console.log("after", contextMenu.style.display);
 }
+
+
+var els = document.querySelectorAll('#context-menu div');
+console.log(els);
+for (var i=0;i<els.length;i++)
+{
+    var contextMenu = document.getElementById("context-menu");
+    els[i].onclick = function(e){
+        contextMenu.style.display = 'none';
+        console.log(e.target);
+    }
+}
+
+
+
+
